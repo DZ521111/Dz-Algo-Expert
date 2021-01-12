6efdb1abb29774cace3afd8a575e3e3e6de88269
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-12 19:14:47 +0530

    Two Number My Solution Uploaded!

---
+++ Two Number Sum/solution1.py
@@ -0,0 +1,20 @@
+'''
+Author : Dhruv B Kakadiya
+
+'''
+
+def twoNumberSum(array, targetSum):
+    # Write your code here.
+	array.sort()
+	front_side, back_side = 0, len(array) - 1
+	while (True):
+		if ((array[front_side] + array[back_side]) == targetSum):
+			return ([array[front_side], array[back_side]])
+		elif ((array[front_side] + array[back_side]) < targetSum):
+			front_side += 1
+			if (front_side > back_side):
+				return ([])
+		elif ((array[front_side] + array[back_side]) > targetSum):
+			back_side -= 1
+			if (front_side > back_side):
+				return ([])
