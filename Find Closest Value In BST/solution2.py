91ccd09d84ad5f55757869ee492fa2abf73dd33d
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-13 10:22:39 +0530

    find closest value in BST updated!

---
+++ Find Closest Value In BST/solution2.py
@@ -0,0 +1,28 @@
+'''
+Author : Dhruv B Kakadiya
+
+'''
+# O(nlog(n)) time | O(1) space | iterative
+
+def findClosestValueInBst(tree, target):
+    # Write your code here.
+    closest = tree.value
+    currNode = tree
+    while (currNode is not None):
+        if (abs(target - currNode.value) < abs(closest - target)):
+            closest = currNode.value
+        if (target < currNode.value):
+            currNode = currNode.left
+        elif (target > currNode.value):
+            currNode = currNode.right
+        else:
+            return closest
+	return closest
+
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
