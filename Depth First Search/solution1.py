00d3cb29beb6e7b04c1f6c8191b94863846249b6
DZ521111 <dhruvkakadiya3@gmail.com>
2021-01-14 12:54:35 +0530

    Depth First Search code updated!

---
+++ Depth First Search/solution1.py
@@ -0,0 +1,25 @@
+'''
+Author : Dhruv B Kakadiya
+
+'''
+# O(v + e) time | O(v) space
+
+# Do not edit the class below except
+# for the depthFirstSearch method.
+# Feel free to add new properties
+# and methods to the class.
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        # Write your code here.
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return (array)
